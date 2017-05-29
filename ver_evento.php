3681986da8c94e66bee816b576ec0055316091f3
aabundis <alanjiraya77@gmail.com>
2017-05-29 10:10:10 -0500

    Inicial

--- ver_evento.php
+++ ver_evento.php
@@ -4,29 +4,45 @@
             <div id="form">
                 <div id="contenedor_img">
                     <img src="" alt="" class="img_evento">
-                    <p id="btn_img">Agregar Imagen</p>
                 </div>
-
                 <div id="contenedor_formulario">
-
-                    <input class="input_form" type="text" id="form_nombre" placeholder="Nombre">
-                    <input class="input_form" type="text" id="form_estado" placeholder="Estado">
-                    <input class="input_form" type="text" id="form_ciudad" placeholder="Ciudad">
-                    <input class="input_form" type="text" id="form_dir" placeholder="Direccion">
-                    <input class="input_form" type="text" id="form_lugar" placeholder="Lugar">
-                    <input class="input_form" type="date" id="form_fecha" placeholder="Fecha">
-                    <input class="input_form" type="time" id="form_hora" placeholder="Hora">
-                    <select class="input_form" id="form_cat"  >
-                        <option value="0">Categoria</option>
-                        <option value="1">Musica</option>
-                        <option value="2">Teatro</option>
-                        <option value="3">Comedia</option>
-                        <option value="4">Actuacion</option>
-                        <option value="5">Magia</option>
-                    </select>
-                    <textarea name="descripcion" id="form_desc" cols="30" rows="5" placeholder="Descripción"></textarea>
-                    <p class="button_form" id="cancelar">Cancelar</p>
-                    <p class="button_form" id="crear">Crear</p>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Nombre:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Estado:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Ciudad:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Direccion:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Lugar:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Fecha:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Hora:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont">
+                        <p class="txt_titulo">Categoria:</p>
+                        <p class="txt_contenido"></p>
+                    </div>
+                    <div class="txt_cont" id="txt_contdes">
+                        <p class="txt_titulo" id="txt_titulodes">Descripcion:</p>
+                        <p class="txt_contenido" id="txt_contenidodes"></p>
+                    </div>
+                    <p class="button_comprar" id="comprar">Comprar Boletos</p>
                 </div>
 
             </div>
