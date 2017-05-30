ce344b403ef255c84abdc42ca5e3ca45840c869b
Elias Rosales <mansion1@hotmail.com>
2017-05-30 10:16:55 -0500

    ventana info_usuario

--- info_usuario.php
+++ info_usuario.php
@@ -3,26 +3,16 @@
         <row>
             <form id="info_usuario">
                 <div id="contenedor_img">
-                    <img src="" alt="" class="img_evento">
-                    <img src="img/s.png" id="edit_img2">
-                    <img src="img/p.png" id="edit_img">
-                    <input class="input_form" type="text" id="input_img" placeholder="Ingrese la URL de la imagen" required>
+
                 </div>
 
                 <div id="contenedor_info_usuario">
-                    <img src="img/p.png" id="img_1">
-                    <img src="img/s.png" id="img_11">
                     <p class="p_info" id="info_nombre">Nombres</p>
-                    <img src="img/p.png" id="img_2">
-                    <img src="img/s.png" id="img_21">
                     <p class="p_info" id="info_correo">coreo@vaaqui.com</p>
-                    <img src="img/p.png" id="img_3">
-                    <img src="img/s.png" id="img_31">
                     <p class="p_info" id="info_direccion">ubicacion #16</p>
-                    <img src="img/p.png" id="img_4">
-                    <img src="img/s.png" id="img_41">
                     <p class="p_info" id="info_telefono">4545123548</p>
-
+                    <button class="act_button" id="act_actualizar">Actualizar</button>
+                    <button class="act_button" id="act_aceptar">Aceptar</button>
                 </div>
 
             </form>
@@ -30,55 +20,15 @@
     </div>
 <?php include 'footer.php'; ?>
 <script>
-    $("#edit_img").click(function(event) {
-        $("#input_img").css('display', 'block');
-        $("#edit_img2").css('display', 'block');
-        $("edit_img").css('display','none');
+    $("#act_actualizar").click(function(event) {
+        $("#act_aceptar").css('display', 'block');
+        $("#act_actualizar").css('display', 'none');
+    });
+    $("#act_aceptar").click(function(event) {
+        $("#act_aceptar").css('display', 'none');
+        $("#act_actualizar").css('display', 'block');
     });
 
-     $("#edit_img2").click(function(event) {
-        $("#input_img").css('display', 'none');
-        $("#edit_img2").css('display', 'none');
-        $("#edit_img").css('display','block');
-    });
 
-      $("#img_1").click(function(event) {
-        $("#img_11").css('display', 'block');
-        $("#img_1").css('display','none');
-    });
 
-     $("#img_11").click(function(event) {
-        $("#img_11").css('display', 'none');
-        $("#img_1").css('display','block');
-    });
-
-    $("#img_2").click(function(event) {
-        $("#img_21").css('display', 'block');
-        $("#img_2").css('display','none');
-    });
-
-     $("#img_21").click(function(event) {
-        $("#img_21").css('display', 'none');
-        $("#img_2").css('display','block');
-    });
-
-      $("#img_3").click(function(event) {
-        $("#img_31").css('display', 'block');
-        $("#img_3").css('display','none');
-    });
-
-     $("#img_31").click(function(event) {
-        $("#img_31").css('display', 'none');
-        $("#img_3").css('display','block');
-    });
-
-      $("#img_4").click(function(event) {
-        $("#img_41").css('display', 'block');
-        $("#img_4").css('display','none');
-    });
-
-     $("#img_41").click(function(event) {
-        $("#img_41").css('display', 'none');
-        $("#img_4").css('display','block');
-    });
 </script>
