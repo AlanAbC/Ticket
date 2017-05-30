32205ace09002a85c653c5efdba65a3878bb7d9b
Elias Rosales <mansion1@hotmail.com>
2017-05-30 08:21:42 -0500

    ventana info_usuario

---
+++ info_usuario.php
@@ -0,0 +1,25 @@
+<?php include 'header.php'; ?>
+    <div class="small-12 columns" id="contenedor">
+        <row>
+            <form id="form">
+                <div id="contenedor_img">
+                    <img src="" alt="" class="img_evento">
+                    <input class="input_form" type="text" id="form_img" placeholder="Ingrese la URL de la imagen" required>
+                </div>
+
+                <div id="contenedor_info_usuario">
+                    <img src="img/p.png" id="img_edit">
+                    <p class="p_info" id="info_nombre">Nombre</p>
+                    <img src="img/p.png" id="img_edit">
+                    <p class="p_info" id="info_correo">coreo@vaaqui.com</p>
+                    <img src="img/p.png" id="img_edit">
+                    <p class="p_info" id="info_direccion">ubicacion #16</p>
+                    <img src="img/p.png" id="img_edit">
+                    <p class="p_info" id="info_telefono">4545123548</p>
+
+                </div>
+
+            </form>
+        </row>
+    </div>
+<?php include 'footer.php'; ?>
