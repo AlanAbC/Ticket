c59e2a4588577614e6041f424916c1d40af5681e
aabundis <alanjiraya77@gmail.com>
2017-05-30 20:02:15 -0500

    Inicial

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/style_login.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.4/sweetalert2.min.css">
+	<link rel="stylesheet" href="css/sweetalert2.min.css">
 	</head>
 	<body style="background-image: url('img/login.jpg');">
 	<div class="small-12 columns" id="mascara">
@@ -21,7 +21,7 @@
 		</row>
 	</div>
     <script src="js/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.4/sweetalert2.min.js"></script>
+    <script src="js/sweetalert2.min.js"></script>
     <script src="js/login.js"></script>
 
 	</body>
